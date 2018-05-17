fbbba73ac1d6c97d022acb0748575126997ad8e8
Victor Costa <victor.costa.23a@gmail.com>
2018-05-17 13:06:58 -0300

    arielleIndex adicionado

---
+++ Developer/Scrapy/Arielle/arielleIndex.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+from arielleProd import raspagens
+import json
+
+url = 'http://www.arielle.com.br/produtos/listar?infinite=true'
+
+lista_url_produto = []
+#arq_json = open('Arielle.json', 'w', encoding='utf-8')
+
+for i in range(0, 7):
+
+    parm = {'pag':str(i)}
+
+    r = requests.get(url, params=parm)
+
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    url_prod = soup.find_all('div', class_='produto')
+
+    for url_p in url_prod:
+        url_produto = url_p.a.get('href')
+        lista_url_produto.append(url_produto)
+
+responde = raspagens(lista_url_produto)
+
+data_json = json.dumps(responde)
+
+arq_json = open('Arielle.json', 'a', encoding='utf-8')
+arq_json.write(data_json)
+
+arq_json.close()
+
+arq_json.close()
